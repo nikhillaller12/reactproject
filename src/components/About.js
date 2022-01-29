bfcc27e61b2d547ec07972e15237adaff8a6a5f7
Nikhil <nikhillaller0@gmail.com>
2022-01-29 23:46:04 +0530

    enjoy

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default function About(props) {
         </h2>
         <div id="collapseOne" style={{background:props.mode==='dark'?'grey':'white',color:props.mode==='light'?'black':'white'}} className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            <strong>This is the first item's acordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
             </div>
         </div>
         </div>
